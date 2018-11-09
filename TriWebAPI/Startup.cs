e7cebbb11ee8183c596bcb97c2a7fca5c3bdb584
peterhu0929 <peterhu0929@gmail.com>
2018-11-09 15:32:30 +0800

    modify startup for CORS

--- TriWebAPI/Startup.cs
+++ TriWebAPI/Startup.cs
@@ -31,6 +31,17 @@ namespace TriWebAPI
             services.AddSingleton<IRepository, Repository>();
             services.AddTransient<DataAccess>();
             services.AddMvc();
+            services.AddCors(options =>
+            {
+                // CorsPolicy 是自訂的 Policy 名稱
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin().SetPreflightMaxAge(new TimeSpan(86400))
+                             .AllowAnyHeader()
+                             .AllowAnyMethod()
+                             .AllowCredentials();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,10 +50,12 @@ namespace TriWebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsPolicy");
             }
             else
             {
                 app.UseHsts();
+                app.UseCors("CorsPolicy");
             }
 
             app.UseHttpsRedirection();
