a2a7e51ea4d7c4df925a84e4797103680db3c33f
peterhu0929 <peterhu0929@gmail.com>
2018-11-09 15:31:39 +0800

    first commit

---
+++ TriWebAPI/Models/DataAccess.cs
@@ -0,0 +1,85 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+
+namespace TriWebAPI.Models
+{
+    public class DataAccess
+    {
+        MongoClient _client;
+        MongoServer _server;
+        MongoDatabase _db;
+
+        MongoClient _clientAzure;
+        MongoServer _serverAzure;
+        MongoDatabase _dbAzure;
+
+        string connectionString =
+  @"mongodb://angular-tri:BgoQgavrKYsD34H1009QjoSXA2ETiqgvZBWKnp0MqtNurKOnKGmqHAWHiK8sThwX3BKuQffl5m1uWC0tbWDB5A==@angular-tri.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+
+        string defaultDBstring = "mongodb://localhost:27017";
+
+        public DataAccess()
+        {
+            _client = new MongoClient(defaultDBstring);
+            _server = _client.GetServer();
+            //_db = _server.GetDatabase("TriDB");
+
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            _clientAzure = new MongoClient(settings);
+            _serverAzure = _clientAzure.GetServer();
+            _db = _serverAzure.GetDatabase("TriDB");
+        }
+
+        public IEnumerable<Triathlon> GetTris()
+        {
+            //return _dbAzure.GetCollection<User>("Users").FindAll();
+            return _db.GetCollection<Triathlon>("Triathlon").FindAll();
+        }
+
+        public IEnumerable<Triathlon> GetTri(string id, string dateS, string dateE, string place)
+        {
+            int i = 0;
+            var qc = new IMongoQuery[4];
+            if (id != "") qc[i++] = Query<Triathlon>.EQ(p => p.Id, new ObjectId(id));
+            if (dateS != "") qc[i++] = Query<Triathlon>.GTE(p => p.date, dateS);
+            if (dateE != "") qc[i++] = Query<Triathlon>.LTE(p => p.date, dateE);
+            if (place != "") qc[i++] = Query<Triathlon>.Matches(p => p.place, place);
+            qc = qc.Where(c => c != null).ToArray();
+            if (qc.Length > 0)
+                return _db.GetCollection<Triathlon>("Triathlon").Find(Query.And(qc));
+            else
+                return _db.GetCollection<Triathlon>("Triathlon").FindAll();
+        }
+
+        public Triathlon CreateTri(Triathlon tri)
+        {
+            tri.in_date = DateTime.Now;
+            tri.up_date = DateTime.Now;
+            _db.GetCollection<Triathlon>("Triathlon").Save(tri);
+            return tri;
+        }
+
+        public bool UpdateTri(ObjectId id, Triathlon tri)
+        {
+            tri.Id = id;
+            tri.up_date = DateTime.Now; 
+            var res = Query<Triathlon>.EQ(u => u.Id, id);
+            var operation = Update<Triathlon>.Replace(tri);
+            var result = _db.GetCollection<Triathlon>("Triathlon").Update(res, operation);
+            return !result.HasLastErrorMessage;
+        }
+        public bool RemoveTri(ObjectId id)
+        {
+            var res = Query<Triathlon>.EQ(e => e.Id, id);
+            var operation = _db.GetCollection<Triathlon>("Triathlon").Remove(res);
+            return !operation.HasLastErrorMessage;
+        }
+    }
+}
